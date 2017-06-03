c0405312ca9eab358049da61412444a2ada6ba72
Anas Khan <anas.khan96@outlook.com>
2017-06-04 04:13:33 +0530

    added check for base32

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ function isBase64URL(text, noPadding) {
     }
     return pattern.test(text);
 }
+
+function isBase32(text, noPadding) {
+    var pattern = /^([A-Z2-7]{8})*([A-Z2-7]{8}|[A-Z2-7]{7}=|[A-Z2-7]{5}===|[A-Z2-7]{4}====|[A-Z2-7]{2}======)$/;
+    noPadding = noPadding || false;
+    if (noPadding) {
+        var patternStr = pattern.source.replace(/=/g, '');
+        pattern = new RegExp(patternStr);
+        return pattern.test(text);
+    }
+    return pattern.test(text);
+}
+
+function isBase32Hex(text, noPadding) { 
+    // TODO
+}
